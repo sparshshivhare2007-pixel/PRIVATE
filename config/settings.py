44e6c2d790f3ccf4ce62c6ef3a1078f8de2d60bb
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-03-30 16:43:59 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,11 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 ADMIN_IDS = [int(id) for id in os.getenv('ADMIN_IDS', '').split(',') if id]
 UPDATE_CHANNEL = os.getenv('UPDATE_CHANNEL')
 LOGS_CHANNEL = os.getenv('LOGS_CHANNEL')
+
+# Payment
 UPI_ID = os.getenv('UPI_ID')
 BINANCE_PAY_ID = os.getenv('BINANCE_PAY_ID')
 USDT_ADDRESS = os.getenv('USDT_ADDRESS')
+
+# Support
+SUPPORT_USERNAME = os.getenv('SUPPORT_USERNAME', 'YourSupport')  # ← YEH LINE ADD KARO
