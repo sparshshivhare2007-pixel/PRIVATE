de01de70e3ff04b3fa52eaf74a4105475d394a77
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-03-30 17:45:17 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -3,10 +3,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Bot
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 BOT_USERNAME = os.getenv('BOT_USERNAME')
 DATABASE_URL = os.getenv('DATABASE_URL')
+
+# Admin
 ADMIN_IDS = [int(id) for id in os.getenv('ADMIN_IDS', '').split(',') if id]
+ADMIN_GROUP_ID = int(os.getenv('ADMIN_GROUP_ID', '0'))  # ← ADD THIS LINE
+
+# Channels
 UPDATE_CHANNEL = os.getenv('UPDATE_CHANNEL')
 LOGS_CHANNEL = os.getenv('LOGS_CHANNEL')
 
@@ -16,4 +22,4 @@ BINANCE_PAY_ID = os.getenv('BINANCE_PAY_ID')
 USDT_ADDRESS = os.getenv('USDT_ADDRESS')
 
 # Support
-SUPPORT_USERNAME = os.getenv('SUPPORT_USERNAME', 'YourSupport')  # ← YEH LINE ADD KARO
+SUPPORT_USERNAME = os.getenv('SUPPORT_USERNAME', 'YourSupport')
